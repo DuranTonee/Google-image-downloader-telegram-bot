3b7ed93b2bc7855ed57ac12514499fa43d458219
Damir <durantoneeet22@gmail.com>
2023-11-23 17:21:57 +0600

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from aiogram.utils import executor
 import telegram_bot
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from create_bot import dp
 
 telegram_bot.register_handlers(dp)
